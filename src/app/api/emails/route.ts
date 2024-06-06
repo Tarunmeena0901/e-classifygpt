f0b00721e977cd74415055be08f43f8f1e5bd9a8
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-06-07 01:45:33 +0530

    implemented email fetching route

---
+++ src/app/api/emails/route.ts
@@ -0,0 +1,50 @@
+import { google } from 'googleapis';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
+import { NEXT_AUTH } from '@/app/config/auth';
+
+
+export async function GET(req: NextApiRequest) {
+  const session = await getServerSession(NEXT_AUTH);
+
+  if (!session) {
+    return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+  }
+
+  const { accessToken } = session.user;
+
+  const oAuth2Client = new google.auth.OAuth2();
+  oAuth2Client.setCredentials({ access_token: accessToken });
+
+  const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+
+  try {
+
+    const response = await gmail.users.messages.list({
+      userId: 'me',
+      q: 'from:twitter.com',
+      maxResults: 25,
+    });
+
+    const messages = response.data.messages || [];
+
+    const emailDetails = await Promise.all(
+      messages.map(async (message) => {
+        const msg = await gmail.users.messages.get({
+          userId: 'me',
+          id: message.id,
+        });
+        const headers = msg.data.payload.headers;
+        const subjectHeader = headers.find(header => header.name === 'Subject');
+        const subject = subjectHeader ? subjectHeader.value : 'No Subject';
+        return { id: message.id, subject };
+      })
+    );
+
+    return NextResponse.json({ emailDetails }, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching emails', error);
+    return NextResponse.json({ error: 'Failed to fetch emails' }, { status: 500 });
+  }
+}
