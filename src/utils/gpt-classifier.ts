df2068eb8edc8723b60558084286fde1f43bd031
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-06-08 02:40:42 +0530

    minor prompt fix

--- src/utils/gpt-classifier.ts
+++ src/utils/gpt-classifier.ts
@@ -23,7 +23,7 @@ export async function classifyEmails(emails : EMAIL[]) {
         messages: [
           {
             role: "user",
-            content: ` judging from the subject line is this email important or not? \n ${email.subject} \n giver answer as important , promotion , social , marketing , spam or general if none are matched, give answer from these options only  `,
+            content: ` judging from the subject line is this email important or not? \n ${email.subject} \n giver answer as "important" , "promotion" , "social" , "marketing" , "spam" or "general" if none are matched, give one word answer from these options only  `,
           },
         ],
         model: "gpt-3.5-turbo",
