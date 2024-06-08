a7b88a4828da7df50bf09ed0b153321daec8a545
Tarunmeena0901 <meenatarun0901@gmail.com>
2024-06-08 18:22:45 +0530

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # e-classifygpt
-email classifier using OpenAi api
+
+![alt text](image-1.png)
+
+the website use next-auth for authentication with google as the provider , for fetching gmail we are using google gmail api , and the email classification done using gpt-4o model additionaly the website have framer-motino as a dependecy to add some smooth transition and animation 
+
+## Prerequisites
+
+Before getting started, ensure you have the following installed:
+
+- Node.js and npm ([installation guide](https://nodejs.org/))
+- Git ([installation guide](https://git-scm.com/))
+- Google Cloud Platform Account with API access enabled : https://developers.google.com/workspace/guides/create-credentials
+- OpenAI API Key : https://platform.openai.com/signup
+
+## Getting Started
+
+1. **Clone the repository:**
+
+```bash
+git clone https://github.com/Tarunmeena0901/e-classifygpt.git
+```
+
+2. **Install dependencies:**
+cd e-classifygpt
+`npm install` or `yarn install`
+
+3. **setup your secrets and env varaible**
+- first copy the .env.local.example
+- rename it to .env.local
+- fill your secrets
+
+![alt text](image.png)
+
+4. **Run your project localy**
+```bash
+ npm run dev
+ or 
+ yarn dev 
+```
+Your Next.js application should now be running locally at http://localhost:3000.
+
+Additional Resources
+[Next.js Documentation](https://nextjs.org/docs)
+[Google Cloud Documentation](https://cloud.google.com/api-keys/docs/overview)
+[OpenAI API Documentation](https://beta.openai.com/docs/)
--- src/app/components/skeletonLoader.tsx
+++ src/app/components/skeletonLoader.tsx
@@ -12,36 +12,34 @@ export function SkeletonLoader() {
             <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[60vh] animate-pulse ">
             </div>
         </div>
-        <div className="flex flex-col cursor-pointer gap-5 border-2 bg-white rounded border-slate-200 mt-4 h-[20vh] p-5 animate-pulse ">
+        <div className="flex flex-col cursor-pointer gap-5 border-2 bg-white rounded border-slate-200 mt-4 h-[20vh] p-5 ">
             <div className="flex justify-between ">
-                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[50vh] border-2 border-slate-50 ">
+                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[50vh] border-2 border-slate-50 animate-pulse  ">
                 </div>
-                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[20vh] ">
+                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[20vh] animate-pulse  ">
                 </div>
             </div>
-            <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[60vh] ">
-
+            <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[60vh] animate-pulse ">
             </div>
         </div>
-        <div className="flex flex-col cursor-pointer gap-5 border-2 bg-white rounded border-slate-200 mt-4 h-[20vh] p-5 animate-pulse ">
+        <div className="flex flex-col cursor-pointer gap-5 border-2 bg-white rounded border-slate-200 mt-4 h-[20vh] p-5 ">
             <div className="flex justify-between ">
-                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[50vh] border-2 border-slate-50 ">
+                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[50vh] border-2 border-slate-50 animate-pulse  ">
                 </div>
-                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[20vh] ">
+                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[20vh] animate-pulse  ">
                 </div>
             </div>
-            <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[60vh] ">
-
+            <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[60vh] animate-pulse ">
             </div>
         </div>
-        <div className="flex flex-col cursor-pointer gap-5 border-2 bg-white rounded border-slate-200 mt-4 h-[20vh] p-5 animate-pulse ">
+        <div className="flex flex-col cursor-pointer gap-5 border-2 bg-white rounded border-slate-200 mt-4 h-[20vh] p-5 ">
             <div className="flex justify-between ">
-                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[50vh] border-2 border-slate-50 ">
+                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[50vh] border-2 border-slate-50 animate-pulse  ">
                 </div>
-                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[20vh] ">
+                <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[20vh] animate-pulse  ">
                 </div>
             </div>
-            <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[60vh] ">
+            <div className=" rounded-full bg-gradient-to-r from-slate-500 h-[4vh] w-[60vh] animate-pulse ">
             </div>
         </div>
     </div>
